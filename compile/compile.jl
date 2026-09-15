454f62299efb73b4cba5380c8c5fb279b53fe459
FRUrgorri <fernando.roca@ciemat.es>
2026-09-15 11:22:12 +0200

    Update of the compile pkgs and workflow

--- compile/compile.jl
+++ compile/compile.jl
@@ -4,6 +4,7 @@ using Pkg
 #This does not include GridapMHD since it has a __precompile(false)___ which is incompatible with creating the sysimage
 pkgs = Symbol[]
 append!(pkgs, [Symbol(v.name) for v in values(Pkg.dependencies()) if v.is_direct_dep]) 
+deleteat!(pkgs,findall(isequal(:GridapMHD),pkgs))
 
 create_sysimage(pkgs,sysimage_path="compile/GridapMHDCalculations.so",precompile_execution_file="compile/compile_workflow.jl", sysimage_build_args=`-O3 --check-bounds=no`)
 #create_sysimage(pkgs,sysimage_path="compile/GridapMHDCalculations.so",precompile_execution_file="compile/compile_mpi_workflow.jl",sysimage_build_args=`-O3 --check-bounds=no`)
--- compile/compile_workflow.jl
+++ compile/compile_workflow.jl
@@ -1,4 +1,5 @@
-using GridapMHDCalculations.serialTest: Run_test_channel, Run_test_multigrid
+using GridapMHDCalculations.serialTest: Run_test_channel, Run_test_channel_blocks, Run_test_multigrid
 
-Run_test_channel(;b=1, Ha=10.0)
-Run_test_multigrid(;b=1, Ha=10.0)
+Run_test_channel(;b=1.0, Ha=10.0)
+Run_test_channel_blocks(;b=1.0, Ha=10.0)
+Run_test_multigrid(;b=1.0, Ha=10.0)
