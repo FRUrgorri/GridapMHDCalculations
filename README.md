9f05aa42133687cfedde86bb2838dfd19f21a5e0
FRUrgorri <fernando.roca@ciemat.es>
2026-09-15 11:20:40 +0200

    The dependency to GridapMHD has been updated to a branch of the fork (upstream-mirror) instead of the previus "include" approach

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # GridapMHDCalculations
 
- Julia Project for launching GridapMHD calculations under different enviroments  
+ Julia Project for launching simulations using GridapMHD app.
+ 
+ Since GridapMHD is currently unregister the dependency needs to be added when instantiating the project. 
+ Current is based on:
+
+    add https://github.com/FRUrgorri/GridapMHD.jl.git#mirror-upstream
+
+Note: The current "__precompile(false)__" of GridapMHD prevents the precompilation of GridapMHDCalculations (which returns a ?). 
--- src/GridapMHDCalculations.jl
+++ src/GridapMHDCalculations.jl
@@ -2,8 +2,10 @@ module GridapMHDCalculations
 
 using Revise
 
-include("../../master/GridapMHD.jl/src/GridapMHD.jl")       #TDB: Add a global path or something along those lines
-using .GridapMHD: main, default_solver_params, uses_petsc     #.GridapMHD = GridapMHDCalculations.GridapMHD This is necessary to get the exports from GridapMHD
+#include("../../master/GridapMHD.jl/src/GridapMHD.jl")       #TDB: Add a global path or something along those lines
+#using .GridapMHD: main, default_solver_params, uses_petsc     #.GridapMHD = GridapMHDCalculations.GridapMHD This is necessary to get the exports from GridapMHD
+
+using GridapMHD: main, default_solver_params, uses_petsc
 
 using PartitionedArrays
 using SparseArrays
